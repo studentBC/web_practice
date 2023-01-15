2b7286ded5045e75b66fb9cde513bdae59856e51
studentBC <r04546032@ntu.edu.tw>
2023-01-15 15:16:44 -0800

    all previous function works now
    we are going to change the UI and add the new feature to
    match hw8 now

--- hw8/server/server.js
+++ hw8/server/server.js
@@ -1,73 +1,84 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+//https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+//https://stackoverflow.com/questions/23751914/how-can-i-set-response-header-on-express-js-assets
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 function SendRequest(datatosend) {
-    function OnResponse(response) {
-        var data = '';
+  function OnResponse(response) {
+    var data = '';
 
-        response.on('data', function(chunk) {
-            data += chunk; //Append each chunk of data received to this variable.
-        });
-        response.on('end', function() {
-            console.log(data); //Display the server's response, if any.
-        });
-    }
+    response.on('data', function (chunk) {
+      data += chunk; //Append each chunk of data received to this variable.
+    });
+    response.on('end', function () {
+      console.log(data); //Display the server's response, if any.
+    });
+  }
 
-    var request = http.request(urlparams, OnResponse); //Create a request object.
+  var request = http.request(urlparams, OnResponse); //Create a request object.
 
-    request.write(datatosend); //Send off the request.
-    request.end(); //End the request.
+  request.write(datatosend); //Send off the request.
+  request.end(); //End the request.
 }
 async function callAPI(url) {
-    let res;
-    let api_key = '';
-    await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': api_key,
-            'Content-Type': 'application/json'
-        }
+  let res;
+  let api_key = 'Bearer 14KXrZ0B_akWx-QGszPZVHBNMj2PKWHxd5FMjNVDHQ5Re_fY1fnJWcSijh66KHdu0Zon6yxIyGXiauJvKaTO29TGAcQJ4TzAYwBXMhvzPnqcIltOaIQxQdwSnge7Y3Yx';
+  await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': api_key,
+        'Content-Type': 'application/json'
+      }
     })
     .then(response => response.json())
     .then(response => {
-        res = response;
-        // console.log(response);
-        console.log(JSON.stringify(response));
-        // return response;
+      res = response;
+      // console.log(response);
+      console.log(JSON.stringify(response));
+      // return response;
     });
-    return res;
+  return res;
 }
-app.post('/getYelpSearch', async function(request, response) {
-    console.log('node JS was called ...');
-    let jObj = request.body;
-    console.log('node JS was called ...');
-    console.log(jObj);
-    let url = jObj.url;
-    console.log(url);
-    let jbody = await callAPI(url);
-    console.log(jbody);
-    response.send(jbody);
-    // app.get(url, function(req, res) {
-    //     console.log(req.headers);
-    //     req.headers['Authorization'] = 'Bearer %s' % api_key;
-    //     req.pipe(request(url)).pipe(res);
-    //     console.log(res.body);
-    //     response.headers['Access-Control-Allow-Origin'] = '*';
-    //     response.send(res.body);
-    //     return res.body;
-    // });
-    // let resHeader = {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Content-Type': 'application/json'
-    // }
-    //response.headers['Access-Control-Allow-Origin'] = '*';
-    //response.headers = resHeader;
-    // response.send('cannot get Yelp Data man ...');
+app.post('/getYelpSearch', async function (request, response) {
+  console.log('node JS was called ...');
+  let jObj = request.body;
+  console.log('node JS was called ...');
+  console.log(jObj);
+  let url = jObj.url;
+  console.log(url);
+  let jbody = await callAPI(url);
+  console.log(jbody);
+  // response.set('Access-Control-Allow-Origin','*');
+  //response.headers. Access-Control-Allow-Origin
+  // response.headers['Access-Control-Allow-Origin'] = '*';
+  response.send(jbody);
+  // app.get(url, function(req, res) {
+  //     console.log(req.headers);
+  //     req.headers['Authorization'] = 'Bearer %s' % api_key;
+  //     req.pipe(request(url)).pipe(res);
+  //     console.log(res.body);
+  //     response.headers['Access-Control-Allow-Origin'] = '*';
+  //     response.send(res.body);
+  //     return res.body;
+  // });
+  // let resHeader = {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Content-Type': 'application/json'
+  // }
+  //response.headers['Access-Control-Allow-Origin'] = '*';
+  //response.headers = resHeader;
+  // response.send('cannot get Yelp Data man ...');
 })
 app.get('/', (req, res) => {
-    console.log(req.body);
-    res.send('<h1> Hola !</h1>');
+  console.log(req.body);
+  res.send('<h1> Hola !</h1>');
 })
 app.listen(3000);
--- hw8/src/app/server/server.component.ts
+++ hw8/src/app/server/server.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
-import {NgForm} from "@angular/forms";
-import {HttpClient} from "@angular/common/http"
+import { NgForm } from "@angular/forms";
+import { HttpClient, HttpXsrfTokenExtractor } from "@angular/common/http"
 // platformBrowserDynamic().bootstrapModule(ServerComponent);
 
 declare global {
     var lat: 0;
     var long: 0;
     var res: 0;
-    var jsobj: JSON;
-    var ascending: true,
-    prevCol: -1;
-    var jsonObjArray: [JSON];
+    var jsobj: object;
+    var jsonText: string;
+    var ascending: boolean,
+        prevCol: -1;
+    var jsonObjArray: object[];
+    var jsonStrArray: string[];
 }
 
 @Component({
@@ -18,17 +20,22 @@ declare global {
     templateUrl: './server.component.html',
     styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit{
+export class ServerComponent implements OnInit {
     /////////////// inital variable realted ////////////
     ngOnInit(): void {
-        
+        globalThis.ascending = true;
+        globalThis.jsonObjArray = [];
+        globalThis.jsonStrArray = [];
     }
     constructor(private http: HttpClient) {
 
     }
-    initial(jojo: JSON) {
+    initial(jojo: object) {
+        console.log(typeof (jojo));
+        console.log(jojo);
         globalThis.jsobj = jojo;
-        jsonObjArray.push(jojo);
+        globalThis.jsonObjArray.push(jojo);
+        globalThis.jsonStrArray.push(JSON.stringify(jojo));
     }
     clickTab(id: string) {
         let tabs = document.getElementById("sea");
@@ -52,69 +59,62 @@ export class ServerComponent implements OnInit{
         let xmlHttpReq = new XMLHttpRequest();
         xmlHttpReq.open("GET", theUrl, false);
         xmlHttpReq.send(null);
-        return xmlHttpReq.responseText; 
+        return xmlHttpReq.responseText;
     }
     async callAPI(url: string) {
         console.log('enter callAPI');
         console.log(url);
         //nodeJS server IP
-        // this.http.post('http://127.0.0.1:3000/getYelpSearch', {'url': url}).subscribe (
-        //     res=>{
-        //         console.log(res);
-        //         //this.initial(res);
-        //         //JSON.parse(res);
-        //     }
-        // );
-
-        console.log('------- go next ---------');
         await fetch('http://127.0.0.1:3000/getYelpSearch', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "url": url
-                })
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "url": url
             })
+        })
             .then(response => response.json())
             .then(response => {
+                globalThis.jsonText = JSON.stringify(response);
+                console.log(typeof (response));
                 globalThis.jsobj = response;
-                console.log(typeof(response));
-                console.log(JSON.stringify(response));
+                console.log(typeof (response));
+                //console.log(JSON.stringify(response));
                 return response;
             })
             .then(response => this.initial(response));
         console.log('---- come out ----');
-        // .then(response => createAPIresultTable(response))
-        // .then(response => {
-        // console.log(globalThis.jsobj.total);
+        let jojo = JSON.parse(globalThis.jsonText);
+        console.log(typeof (jojo));
+        console.log(jojo.total);
         // console.log(jsonObjArray[0].total);
-        // let left = globalThis.jsobj.total - globalThis.jsobj.businesses.length;
-        // if (left > 0) {
-        //     let last = left % 50,
-        //         start = 50;
-        //     while (left) {
-        //         await fetch('http://127.0.0.1:5000/getYelpSearch', {
-        //                 method: 'POST',
-        //                 headers: {
-        //                     'Accept': 'application/json',
-        //                     'Content-Type': 'application/json'
-        //                 },
-        //                 body: JSON.stringify({
-        //                     "url": url + '&offset=' + start
-        //                 })
-        //             })
-        //             .then(response => response.json())
-        //             .then(response => this.initial(response))
-        //             .then(response => {
-        //                 start += globalThis.jsobj.businesses.length;
-        //                 left -= globalThis.jsobj.businesses.length;
-        //             });
-        //     }
-        // }
-        // console.log(jsonObjArray.length);
-        // this.createAPIresultTable();
+        let left = jojo.total - jojo.businesses.length, start = 50;
+        if (left > 0) {
+            while (left) {
+                console.log(left);
+                await fetch('http://127.0.0.1:3000/getYelpSearch', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        "url": url + '&offset=' + start
+                    })
+                })
+                    .then(response => response.json())
+                    .then(response => this.initial(response))
+                    .then(response => {
+                        start += jojo.businesses.length;
+                        let job = JSON.parse(globalThis.jsonStrArray[jsonStrArray.length-1]);
+                        left -= job.businesses.length;
+                    });
+            }
+        }
+        console.log(jsonObjArray.length);
+        this.createAPIresultTable();
         return;
     }
     // showPosition(position: JSON) {
@@ -123,7 +123,7 @@ export class ServerComponent implements OnInit{
     //     console.log(lat);
     //     console.log(long);
     // }
-    submitlol(form: NgForm):void {
+    submitlol(form: NgForm): void {
         // console.log(event);
         //form submit will refresh my page and clear log
         // event.preventDefault();
@@ -143,7 +143,7 @@ export class ServerComponent implements OnInit{
         console.log(selfLocate);
         console.log(fc);
         console.log(dist);
-
+        let debug = true;
 
         if (!dist) dist = 10000;
         if (!fc) fc = "all";
@@ -159,6 +159,7 @@ export class ServerComponent implements OnInit{
                 alert("Geolocation is not supported by this browser.");
             }
         } else {
+            if (!debug) {
             loc = loc.replace(/\s+/g, '+');
             var gkey = '';
             var gr = this.httpGet('https://maps.googleapis.com/maps/api/geocode/json?address=' + loc + '&key=' + gkey);
@@ -166,11 +167,240 @@ export class ServerComponent implements OnInit{
             var res = JSON.parse(gr);
             globalThis.lat = res.results[0].geometry.location.lat;
             globalThis.long = res.results[0].geometry.location.lng;
+            }
         }
         //call YELP https://docs.developer.yelp.com/reference/v3_business_search
         //let url = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972&offset=20&limit=50';
-        let url = 'https://api.yelp.com/v3/businesses/search?term=' + kw + '&latitude=' + lat + '&longitude=' + long +
+        let url = "";
+        if (debug) {
+            url = 'https://api.yelp.com/v3/businesses/search?term=delis&location=Austin&limit=50'
+        } else {
+             url = 'https://api.yelp.com/v3/businesses/search?term=' + kw + '&latitude=' + lat + '&longitude=' + long +
             '&categories=' + fc + '&radius=' + dist + '&limit=50';
+        }
         this.callAPI(url);
+    }
+    ///////////// dynamically deal with webUI ///////////////
+    createAPIresultTable() {
+        console.log('enter to createAPIresultTable');
+        //store json obj into data array
+        var len = jsonObjArray.length;
+        console.log(len);
+        let data: any[] = [];
+        if (len > 0) {
+            console.log('enter to creat !!!');
+            const elems = document.getElementsByClassName("APIresult");
+            for (let i = 0; i < elems.length; i++) {
+                const ee = elems[i] as HTMLElement;
+                ee.style.display = 'flex';
+            }
+            let index = 1;
+            console.log('------  here ------');
+            for (let j = 0; j < len; j++) {
+                console.log(jsonStrArray[j]);
+                let jsonObj = JSON.parse(jsonStrArray[j]);
+                console.log('jsonObj length is ' + jsonObj.businesses.length);
+                for (let i = 0; i < jsonObj.businesses.length; i++) {
+                    let tmp = [];
+                    tmp.push(index);
+                    if (jsonObj.businesses[i]?.image_url) tmp.push(jsonObj.businesses[i].image_url);
+                    else tmp.push('http://clipart-library.com/images/dc45Exp9i.png');
+                    if (jsonObj.businesses[i]?.name) tmp.push(jsonObj.businesses[i].name);
+                    else tmp.push('fuck');
+                    if (jsonObj.businesses[i]?.rating) tmp.push(jsonObj.businesses[i].rating);
+                    else tmp.push(-1);
+                    if (jsonObj.businesses[i]?.distance) tmp.push(jsonObj.businesses[i].distance);
+                    else tmp.push(-1);
+                    data.push(tmp);
+                    index++;
+                }
+            }
+            console.log('data length is ' + data.length);
+        }
+        var table = document.createElement('table');
+        table.setAttribute("id", "APIresultTable");
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var td5 = document.createElement('td');
+
+        var text1 = document.createTextNode('No.');
+        var text2 = document.createTextNode('Image');
+        var text3 = document.createTextNode('Business Name');
+        var text4 = document.createTextNode('Rating');
+        var text5 = document.createTextNode('Distance(miles)');
+        tr.classList.add("arow");
+        td1.appendChild(text1);
+        td1.classList.add("no");
+        //td1.addEventListener("click", ()=>sortColumn(0));
+        td2.appendChild(text2);
+        td2.classList.add("img");
+        //td2.addEventListener("click", ()=>sortColumn(1));
+        td3.appendChild(text3);
+        td3.classList.add("bn");
+
+        td3.addEventListener("click", () => this.sortColumn(data, 2));
+        td4.appendChild(text4);
+        td4.classList.add("rating");
+        td4.addEventListener("click", () => this.sortColumn(data, 3));
+        td5.appendChild(text5);
+        td5.classList.add("dist");
+        td5.addEventListener("click", () => this.sortColumn(data, 4));
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+
+        table.appendChild(tr);
+        console.log('before creating table ...');
+        console.log('data length is ' + data.length);
+        for (let i: number = 0; i < data.length; i++) {
+            tr = document.createElement('tr');
+            tr.classList.add("arow");
+            td1 = document.createElement('td');
+            td1.classList.add("no");
+            td2 = document.createElement('td');
+            td2.classList.add("img");
+            //let nav3 = document.createElement('nav');
+            //we shall create <nav> </nav>
+            // nav3.classList.add("nav-link");
+            td3 = document.createElement('td');
+            td3.classList.add("bn");
+            console.log('');
+            // td3.classList.add("nav");
+            //creating a function which will not be executed immediately => moreinfo.bind(null, jsonObj.businesses[i].name)
+            //td3.addEventListener("click", moreInfo.bind(null, jsonObj.businesses[i].name));
+            td3.addEventListener("click", () => this.moreInfo(data[i][2]));
+            td4 = document.createElement('td');
+            td4.classList.add("rating");
+            td5 = document.createElement('td');
+            td5.classList.add("dist");
+
+
+            let num = i + 1;
+            text1 = document.createTextNode(num.toString());
+            let image = document.createElement('img');
+            image.src = data[i][1];
+            image.style.width = "150px";
+
+            text3 = document.createTextNode(data[i][2]);
+            text4 = document.createTextNode(data[i][3]);
+            text5 = document.createTextNode(data[i][4]);
+
+            td1.appendChild(text1);
+            td2.appendChild(image);
+            td3.appendChild(text3);
+            td4.appendChild(text4);
+            td5.appendChild(text5);
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+
+            table.appendChild(tr);
+        }
+        console.log('after creating table ...');
+        document.getElementsByClassName("APIresult")[0].appendChild(table);
+    }
+
+    sortColumn(array: any, col: any) {
+        if (globalThis.prevCol == col) globalThis.ascending = !globalThis.ascending;
+        if (ascending) {
+            array.sort(function (a: any, b: any) {
+                var x = a[col];
+                var y = b[col];
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            });
+        } else {
+            array.sort(function (a: any, b: any) {
+                var x = a[col];
+                var y = b[col];
+                return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+            });
+        }
+        let table = document.getElementById("APIresultTable") as HTMLTableElement;
+        for (let i = 1; i < array.length; i++) {
+            console.log(array[i][0]);
+            console.log(table.rows[i].cells[1].innerHTML);
+            //the image is not inner text so we need to change it!
+            //table.rows[i].cells[1].innerText = array[i][1];
+            table.rows[i].cells[1].innerText = "";
+            let img = document.createElement('img');
+            img.style.width = "150px";
+            img.src = array[i][1];
+            table.rows[i].cells[1].appendChild(img);
+            //the business name still need to show its link
+            table.rows[i].cells[2].innerText = array[i][2];
+            table.rows[i].cells[3].innerText = array[i][3];
+            table.rows[i].cells[4].innerText = array[i][4];
+        }
+        globalThis.prevCol = col;
+    }
+    //when user click the title and ask for more info
+    moreInfo(name: string) {
+        console.log('enter moreInfo');
+        const elems = document.getElementsByClassName("searchResult");
+        for (let i = 0; i < elems.length; i++) {
+            const ee = elems[i] as HTMLElement;
+            ee.style.display = 'flex';
+        }
+
+        for (let a = 0; a < jsonObjArray.length; a++) {
+            let jsobj = JSON.parse(jsonStrArray[a]);
+            for (let i = 0; i < jsobj.businesses.length; i++) {
+                if (name === jsobj.businesses[i].name) {
+                    var addr = "",
+                        trs = "",
+                        cate = "";
+                    for (let j = 0; j < Object.keys(jsobj.businesses[0].location.display_address).length; j++) {
+                        addr += jsobj.businesses[j].location.display_address[j];
+                        addr += " ";
+                    }
+                    console.log(addr);
+                    //document.getElementById("address").textContent = addr;
+                    let tmp =document.getElementById("pn") as HTMLElement;
+                    if (jsobj.businesses[i].phone ) {
+                        tmp.innerHTML = jsobj.businesses[i].phone;
+                    } else {
+                        tmp.innerHTML = "None";
+                    }
+                    tmp = document.getElementById("address") as HTMLElement;
+                    tmp.innerHTML = addr;
+
+                    for (let j = 0; j < jsobj.businesses[i].transactions.length; j++) {
+                        trs += jsobj.businesses[i].transactions[j];
+                        trs += " ";
+                    }
+                    console.log(trs);
+                    tmp = document.getElementById("transactionsSupported") as HTMLElement;
+                    if (trs.length) {
+                        tmp.innerHTML = trs;
+                    } else {
+                        tmp.innerHTML = "None";
+                    }
+                    for (let j = 0; j < jsobj.businesses[i].categories.length; j++) {
+                        cate += jsobj.businesses[i].categories[j].alias;
+                        cate += " ";
+                    }
+
+                    console.log(cate);
+                    let lol = document.getElementById("categories") as HTMLElement;
+                    lol.textContent = cate;
+                    //for more info
+                    //var anchor = document.createElement('a');
+                    let anchor = document.getElementById("minfo") as HTMLAnchorElement;
+                    //var anchorText = document.createTextNode('Yelp');
+                    anchor.href = jsobj.businesses[i].url;
+                    //anchor.setAttribute('href', jsobj.businesses[i].url);
+                    break;
+                }
+            }
+        }
     }
 }
