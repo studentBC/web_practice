c208ec8beb59ed5a65c34ac44cd7c3adba6b244f
studentBC <r04546032@ntu.edu.tw>
2023-01-21 22:50:33 -0800

    initial draft

---
+++ hw9/hw9/controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw9
+//
+//  Created by Chin Lung on 1/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
